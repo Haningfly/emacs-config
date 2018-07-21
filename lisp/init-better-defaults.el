3309b34fe7ab3762467f4902f0dfd51dbb7694ac
Curaa <perfectmatchyi@gmail.com>
2018-07-22 00:58:38 +0800

    split config into lisp/.el

--- lisp/init-better-defaults.el
+++ lisp/init-better-defaults.el
@@ -1,1 +1,50 @@
 ;; 增强内置功能
+
+;; Find Executable Path on OS X
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+;; 自动加载外部修改后的文件
+(global-auto-revert-mode 1)
+
+;; 关闭警告提示音
+(setq ring-bell-function 'ignore)
+
+;; 关闭自动保存
+(setq auto-save-default nil)
+
+;; 开启evil模式
+(evil-mode 1)
+
+;; 快速打开配置文件
+(defun open-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+;; 这一行代码，将函数 open-init-file 绑定到 <f2> 键上
+(global-set-key (kbd "<f2>") 'open-init-file)
+
+; 开启全局 Company 补全
+(global-company-mode 1)
+(setq make-backup-files nil)
+(setq auto-mode-alist
+      (append
+       '(
+	 ("\\.js\\'" . js2-mode)
+	 ("\\.python\\'" . python-mode)
+	 ("\\.go\\'" . go-mode)
+       )
+       auto-mode-alist))
+
+(require 'smex)
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+(require 'org)
+(setq org-src-fontify-natively t)
+
+(setq custom-file "~/.emacs.d/lisp/custom-config.el")
+
+(provide 'init-better-defaults)
--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -1,1 +1,36 @@
 ;; 视觉层配置
+;; 关闭工具栏，tool-bar-mode 即为一个 Minor Mode
+(tool-bar-mode -1)
+
+;; 关闭文件滑动控件
+(scroll-bar-mode -1)
+
+;; 显示行号
+(global-linum-mode 1)
+
+;; 更改光标的样式（不能生效，解决方案见第二集）
+(setq cursor-type 'bar)
+
+;; 关闭启动帮助画面
+(setq inhibit-splash-screen 1)
+
+;; 更改显示字体大小 16pt
+;; http://stackoverflow.com/questions/294664/how-to-set-the-font-size-in-emacs
+(set-face-attribute 'default nil :height 160)
+
+;; 全屏 
+(set-frame-parameter nil 'fullscreen 'fullboth)
+
+;; 括号匹配
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+
+;; 高亮当前行
+(global-hl-line-mode 1)
+
+;; 加载主题配置
+;; (load-theme 'monokai 1)
+(load-theme 'atom-one-dark 1)
+
+(setq powerline-default-theme 1)
+
+(provide 'init-ui)
